808ffff81493d3d9488e96b8aa2a7722ee528a80
AbhishekRathore <65111377+Abhi805@users.noreply.github.com>
2024-08-17 19:14:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './components/Todo'
+import TodoItems from './components/TodoItems'
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 min-h-screen grid py-4'>
+  <Todo />
+
+    </div>
+  )
+}
+
+export default App
